7ca6e71340ac64e4b0e5762697bce33bcd5960af
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-04-23 21:56:31 -0300

    remove function doDemo/clearDemo

--- js/cabo-de-guerra.js
+++ js/cabo-de-guerra.js
@@ -1,31 +1,6 @@
 let square = document.getElementById("square");
 let position = 0;
 
-// function doDemo(button) {
-//     square.style.backgroundColor = "#3a7980";
-//     // square.style.position = "-y 35";
-//     button.setAttribute("disabled", "true");
-//     setTimeout(clearDemo, 2000, button);
-// };
-
-// function clearDemo(button) {
-//     square.style.backgroundColor = "#b00720";
-//     button.removeAttribute("disabled");
-// };
-
-// function move(button) {
-//     const interval = setInterval(() => {
-//         if (position >= 300) {
-//             clearInterval(interval);
-//         } else {
-//             position += 5;
-//             square.style.left = position + "px";
-//         };
-//     }, 30);
-
-//     setTimeout(positionZero, 2000, button);
-// };
-
 function move(button) {
     position += 5;
     square.style.left = position + "px";
@@ -43,3 +18,4 @@ function positionZero(button) {
         };
     }, 30);
 };
+
