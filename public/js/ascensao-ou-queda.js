0fb2a6e2c737148a8bb5faa56429c9a59ac71820
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-05-01 16:54:52 -0300

    Ascensão ou Queda

--- public/js/ascensao-ou-queda.js
+++ public/js/ascensao-ou-queda.js
@@ -1,12 +1,13 @@
 
 const container = document.querySelector('#container');
+const img = document.querySelector('#imgResult');
+const result = document.querySelector('#result');
 let player = document.querySelector('input#player');
-let team = document.querySelector('.team');
+let teamPlayer = document.querySelector('.teamPlayer');
+let teamVilain = document.querySelector('.teamVilain');
 
-let personagem = ["", "", ""];
-
+let personagem = [];
 let viloes = ["", "", ""];
-
 let forcaPersonagem = 0;
 let forcaViloes = 0;
 
@@ -17,64 +18,71 @@ function escolhaJogador() {
         alert(`Nenhum jogador válido!`);
     }
 
-    let labelPlayer = document.createElement('label');
-    labelPlayer.classList.add('namePlayer');
+    if (personagem.length < 3) {
+        let labelPlayer = document.createElement('label');
+        labelPlayer.classList.add('namePlayer');
 
-    let parText = document.createElement('p');
-    parText.innerText = teamValue;
+        let parText = document.createElement('p');
+        parText.innerText = teamValue;
 
-    labelPlayer.appendChild(parText);
-    team.appendChild(labelPlayer);
+        labelPlayer.appendChild(parText);
+        teamPlayer.appendChild(labelPlayer);
 
-    personagem.push(teamValue);
-    strengthLevel(teamValue)
+        personagem.push(teamValue);
+        strengthLevel(teamValue)
 
-    player.value = '';
-    player.focus();
+        player.value = '';
+        player.focus();
 
+        if (personagem.length === 3) {
+            strengthLevelVillain();
+            battleResult()
+        };
+    };
 };
 
 function strengthLevel(name) {
     const forca = Math.floor(Math.random() * 10) + 1;
     forcaPersonagem += forca;
-
-
-    console.log(`Personagem: ${name} | Força gerada: ${forca}`);
-    console.log(`Força total da equipe: ${forcaPersonagem}`);
-    console.log(`Personagens: ${personagem}` );
 };
-
-
-// function strengthLevel2(escolhaJogador) {
-//     for (let i = 0; i < 3; i++) {
-//         // let escolhaPersonagem = prompt(`Digite o nome do seu personagem ${i + 1}`);
-//         let escolhaPersonagem = `teamValue ${i + 1}`;
-//         personagem[i] = escolhaPersonagem;
-//         forcaPersonagem = forcaPersonagem + 3;
-//         // Math.floor(Math.random() * 10) + 1;
-//         console.log('força1', escolhaPersonagem, forcaPersonagem);
-//     };
-// };
 
 function strengthLevelVillain() {
+    forcaViloes = 0;
+    let viloesPossiveis = ["Thanos", "Lex Luthor", "Voldemort", "Bellatrix Lestrange", "Harley Quinn", "Cruella De Vil", "Duende Verde", "Big Mom", "Malévola", "Scar"];
+
     for (let i = 0; i < 3; i++) {
-        let indiceAleatorio = Math.floor(Math.random() * 10);
-        let viloesPossiveis = ["Thanos", "Lex Luthor", "Voldemort", "Bellatrix Lestrange", "Harley Quinn", "Cruella De Vil", "Duende Verde", "Big Mom", "Malévola", "Scar"];
+        let indiceAleatorio = Math.floor(Math.random() * viloesPossiveis.length);
         viloes[i] = viloesPossiveis[indiceAleatorio];
-        forcaViloes = forcaViloes + Math.floor(Math.random() * 10) + 1;
+        forcaViloes += Math.floor(Math.random() * 10) + 1;
+
+        let labelVilain = document.createElement('label');
+        labelVilain.classList.add('namePlayer');
+
+        let parText = document.createElement('p');
+        parText.innerText = viloes[i];
+
+        labelVilain.appendChild(parText);
+        teamVilain.appendChild(labelVilain);
     };
 };
 
-function result() {
+function battleResult() {
     if (forcaPersonagem > forcaViloes) {
-        alert(`Seu time é muito forte! Você ganhou a disputa de força ! Sua força forca ${forcaPersonagem}`);
+        img.src = "assets/images/ChatGPT-Vitória.png";
+
+        result.innerText = `Seu time é muito forte! \n Você ganhou a disputa de força ! Sua força foi ${forcaPersonagem}`;
+
     } else if (forcaViloes > forcaPersonagem) {
-        alert(`Seu time não foi forte o suficiente. O time dos vilões ganharam a disputa com força de ${forcaViloes}`);
+        img.src = "assets/images/ChatGPT-Derrota.png";
+
+        result.innerText = `Seu time não foi forte o suficiente.\n O time dos vilões ganharam a disputa com força de ${forcaViloes}`;
+
     } else {
-        alert(`Os dois times possuem o mesmo nivel de força! Vocês empataram !!!`);
+        img.src = "assets/images/ChatGPT-hero-vs-vilain-empate.png";
+
+        result.innerText = `Os dois times possuem o mesmo nivel de força!\n Vocês empataram !!!`;
     };
 };
-
 
 document.querySelectorAll(".game-btn").forEach((btn) => {
     btn.addEventListener('click', () => {
